8d0d2696a54d66d4b5e0b693d382d1a8ca99623f
HasibHimel <hasibsdvrt555@gmail.com>
2024-11-13 10:42:46 -0800

    button with image

--- src/com/sdlab/JButtonClass.java
+++ src/com/sdlab/JButtonClass.java
@@ -3,6 +3,7 @@ package com.sdlab;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -14,7 +15,8 @@ public class JButtonClass {
 	    JFrame f=new JFrame("Button Example");  
 	    final JTextField tf=new JTextField();  
 	    tf.setBounds(50,50, 150,20);  
-	    JButton b=new JButton("Click Here");  
+//	    JButton b=new JButton("Click Here"); // Button with text
+	    JButton b=new JButton(new ImageIcon("D:\\eclipse\\swing01\\images\\register_icon.jpg"));// button with image  
 	    b.setBounds(50,100,95,30);  
 	    b.addActionListener(new ActionListener(){  
 	    	public void actionPerformed(ActionEvent e){  
